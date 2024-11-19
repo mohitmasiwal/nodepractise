4f2aa60e56e658bd633d3837565feec713cca811
mohitmasiwal <mohitmasiwal02@gmail.com>
2024-11-19 16:32:50 +0530

    not

--- fileSystem.js
+++ fileSystem.js
@@ -1,13 +1,13 @@
  
 const fs = require("fs");
 
-// fs.readFile("read.txt", "utf8", (err, data) => {
-//     if (err) {
-//         console.log("Error in reading file:", err);
-//     } else {
-//         console.log("File content:", data);
-//     }
-// });
+fs.readFile("read.txt", "utf8", (err, data) => {
+    if (err) {
+        console.log("Error in reading file:", err);
+    } else {
+        console.log("File content:", data);
+    }
+});
 
 
  const data =fs.readFileSync("read.txt","utf8")
