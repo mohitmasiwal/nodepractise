d4aad72bf5bd5a8672f379580d809123d4217d78
mohitmasiwal <mohitmasiwal02@gmail.com>
2024-11-20 16:17:41 +0530

    dbew

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
- // Import the HTTP module
+ 
 const http = require("http");
 
  
 const PORT = 3000;
 
-// Create the server
+ 
 const server = http.createServer((req, res) => {
-    // Set the response header
+     
     res.writeHead(200, { "Content-Type": "text/plain" });
 
-    // Send a response based on the requested URL
+  
     if (req.url === "/") {
         res.end("Welcome to the Home Page!");
     } else if (req.url === "/about") {
@@ -19,7 +19,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
-// Start the server
+ 
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
