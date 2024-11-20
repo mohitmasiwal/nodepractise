d793122e0b42c3edf04830d69f5439139d947b47
mohitmasiwal <mohitmasiwal02@gmail.com>
2024-11-20 15:44:27 +0530

    new

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const http = require('http');
- const port = 3000;
+ // Import the HTTP module
+const http = require("http");
 
- const myserver = http.createServer((req, res) => {
-    res.end("all good")
+ 
+const PORT = 3000;
 
-    myserver.listen((port,()=>{
-        console.log("sever running");
-        
-    }))
- })
+// Create the server
+const server = http.createServer((req, res) => {
+    // Set the response header
+    res.writeHead(200, { "Content-Type": "text/plain" });
+
+    // Send a response based on the requested URL
+    if (req.url === "/") {
+        res.end("Welcome to the Home Page!");
+    } else if (req.url === "/about") {
+        res.end("This is the About Page.");
+    } else {
+        res.end("404: Page not found.");
+    }
+});
+
+// Start the server
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
