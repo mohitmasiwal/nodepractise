754607341baa330a8e0880c819f8679c418638e2
mohitmasiwal <mohitmasiwal02@gmail.com>
2024-11-23 15:52:02 +0530

    kjggk

--- fileSystem.js
+++ fileSystem.js
@@ -1,14 +1,9 @@
  
+const { log } = require("console");
 const fs = require("fs");
 
-fs.readFile("read.txt", "utf8", (err, data) => {
-    if (err) {
-        console.log("Error in reading file:", err);
-    } else {
-        console.log("File content:", data);
-    }
-});
+fs.readFile("new.txt","UTF-8", (err,data)=>{
+    console.log(data)
+    console.log(err)
+})
 
-
- const data =fs.readFileSync("read.txt","utf8")
- console.log(data)
