a350bfa4d4177722da2336eda612fbcc067eb044
mohitmasiwal <mohitmasiwal02@gmail.com>
2024-11-24 20:17:11 +0530

    ljb

--- dsa/array.js
+++ dsa/array.js
@@ -1,4 +1,4 @@
-// let arr =[6,3,4,2,1,6,888,5];
+let arr =[6,3,4,2];
 // let max = arr[0]
 // for(let i =1;i<arr.length;i++){
 // if(arr[i]>max){
@@ -7,10 +7,23 @@
 // }
 // console.log(max)
 
-let n = 7;
-let arr1=[];
-for(i=0;i<n;i++){
-    arr1.push(i)
-}
+// let n = 7;
+// let arr1=[];
+// for(i=0;i<n;i++){
+//     arr1.push(i)
+// }
 
-console.log(arr1);
+// console.log(arr1);
+// for(let i = 0;i<arr.length;i++){
+    
+//     for(let j =i; j<=arr.length;j++){
+        
+//         let ar ='';
+//         for(let k =i; k< j; k++){
+//            ar += arr[k];
+
+//         }
+//         console.log(ar);
+        
+//     }
+}
