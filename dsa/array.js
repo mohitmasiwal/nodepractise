294cd19ec6b32c641c7a30b56957131a0a49efd6
mohitmasiwal <mohitmasiwal02@gmail.com>
2024-11-24 20:59:57 +0530

    kbj

--- dsa/array.js
+++ dsa/array.js
@@ -15,33 +15,33 @@
 
 // console.log(arr1);
 // for(let i = 0;i<arr.length;i++){
-    
+
 //     for(let j =i; j<=arr.length;j++){
-        
+
 //         let ar ='';
 //         for(let k =i; k< j; k++){
 //            ar += arr[k];
 
 //         }
 //         console.log(ar);
-        
+
 // //     }
 // }
 
-let arr2 =  [5,2,-4,-5, 3,-1,2,3,1];
-var max= 0;
-for(let i = 0; i < arr2.length; i++){ 
-    for(let j =i; j<arr2.length; j++){
+let arr2 = [5, 2, -4, -5, 3, -1, 2, 3, 1];
+var max = 0;
+for (let i = 0; i < arr2.length; i++) {
+    for (let j = i; j < arr2.length; j++) {
         let sum = 0;
-        for(let k =i; k<=j;k++){
-       sum += arr2[k]
+        for (let k = i; k <= j; k++) {
+            sum += arr2[k]
         }
-        if(sum > max){
+        if (sum > max) {
             max = sum
         }
-       
+
     }
-   
-    
+
+
 }
 console.log(max);
---
+++ dsa/runningsum.js
@@ -0,0 +1,10 @@
+let arr = [1,2,3,4];
+ 
+for(let i=0;i<arr.length;i++) {
+ for(let j=0;j<arr.length;j++) {
+
+    for(k=0;k<arr.length;k++) {
+        
+    }
+ }
+}
