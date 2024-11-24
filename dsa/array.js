44e3c6501d048ea01db1d3f58e2521695f0c06b7
mohitmasiwal <mohitmasiwal02@gmail.com>
2024-11-24 18:47:03 +0530

    new glitch

--- dsa/array.js
+++ dsa/array.js
@@ -1,8 +1,16 @@
-let arr =[6,3,4,2,1,6,888,5];
-let max = arr[0]
-for(let i =1;i<arr.length;i++){
-if(arr[i]>max){
-    max = arr[i];
+// let arr =[6,3,4,2,1,6,888,5];
+// let max = arr[0]
+// for(let i =1;i<arr.length;i++){
+// if(arr[i]>max){
+//     max = arr[i];
+// }
+// }
+// console.log(max)
+
+let n = 7;
+let arr1=[];
+for(i=0;i<n;i++){
+    arr1.push(i)
 }
-}
-console.log(max)
+
+console.log(arr1);
