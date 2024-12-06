361c821ca49bb3ad70f48a218b5305440cfe4ebf
mohitmasiwal <mohitmasiwal02@gmail.com>
2024-12-06 20:02:09 +0530

    kjw

--- dsa/fs.js
+++ dsa/fs.js
@@ -1,17 +1,9 @@
 let arr =[1,2,3,4,5,6]   //[6,1,5,2,4,3]
 let result=[];
-var min = 0;
-var max = 0;
-for(var i=0; i<arr.length; i++){
- for(var j=1; j<arr.length; j++){
-if(arr[i]>arr[max]){
-    arr[max] = arr[i]
+for(var i=0; i<arr.length/2; i++){
+    result.push(arr[arr.length-1-i]);
+    result.push(arr[i])
+ 
 }
-result.push(arr[max]);
-if(arr[i]<arr[min]){
-    arr[min] = arr[i]
-}
-result.push(arr[min])
- }
- console.log(result)
-}
+ console.log(result);
+ 
