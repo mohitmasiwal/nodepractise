3b8a91fb3b29aded84fe5d44aca69f8187a81f9f
mohitmasiwal <mohitmasiwal02@gmail.com>
2024-12-06 20:08:06 +0530

    e33e

--- dsa/fs.js
+++ dsa/fs.js
@@ -1,4 +1,18 @@
-let arr =[1,2,3,4,5,6]   //[6,1,5,2,4,3]
+let arr =[14,3,5,66,7,87,98,112,1]  
+for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j <  arr.length - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          
+            let temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+        }
+    }
+}
+
+
+
+ 
 let result=[];
 for(var i=0; i<arr.length/2; i++){
     result.push(arr[arr.length-1-i]);
