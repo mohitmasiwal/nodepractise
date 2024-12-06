13f8d6be46bd5e6f3264b7143340157d0a7ab4f4
mohitmasiwal <mohitmasiwal02@gmail.com>
2024-12-06 20:17:50 +0530

    oih

--- dsa/fs.js
+++ dsa/fs.js
@@ -1,20 +1,12 @@
 let arr =[14,3,5,66,7,87,98,112,1]  
-for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j <  arr.length - i - 1; j++) {
-        if (arr[j] > arr[j + 1]) {
-          
-            let temp = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = temp;
-        }
-    }
-}
+ arr.sort((a,b)=>a-b);
+ console.log(arr)
 
 
 
  
 let result=[];
-for(var i=0; i<arr.length/2; i++){
+for(let i=0; i<arr.length/2; i++){
     result.push(arr[arr.length-1-i]);
     result.push(arr[i])
  
